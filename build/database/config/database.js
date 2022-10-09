175afb9017ac0e76fd0ff3b850f8450d87abc054
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2022-10-09 09:27:01 -0300

    fix: :rotating_light: Altera aconexão do sequelize
    Agora a conexão ocorre de acordo com o arquivo .env

--- build/database/config/database.js
+++ build/database/config/database.js
@@ -1,11 +1,15 @@
 "use strict";
-require("dotenv/config");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const config = {
     username: process.env.DB_USER || '',
     password: process.env.PASSWORD || '',
     database: process.env.DB_NAME || '',
     host: process.env.HOST || '',
-    port: Number(process.env.PORT) || 5432,
-    dialect: 'postgres',
+    port: Number(process.env.PORT) || 3306,
+    dialect: 'mysql',
 };
 module.exports = config;
