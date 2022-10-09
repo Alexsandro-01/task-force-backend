b178d3d0e1d01379f1ba9ab6ef92a73ed40185db
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2022-10-09 10:48:51 -0300

    refactor: :recycle: Implementa zod nas instefaces de user
    Para facilitar a checagem e validação dos dados nos tipos de dados esperados nos types de ILogin e IUser.

--- src/interfaces/IUser.ts
+++ src/interfaces/IUser.ts
@@ -1,13 +1,53 @@
-export interface ILogin {
-  email: string,
-  password: string,
-}
+/* eslint-disable camelcase */
+import { z } from 'zod';
 
-export interface IUser {
-  name: string,
-  email: string,
-  password: string,
-}
+export const LoginZodSchema = z.object({
+  email: z.
+  string({
+    required_error: 'Email is required',
+    invalid_type_error: 'Email must be a string',
+  })
+  .email({ message: 'Invalid email address' })
+  .max(255),
+  password: z
+  .string({
+    required_error: 'Password is required',
+    invalid_type_error: 'Password must be a string',
+  })
+  .min(6, { message: 'Password must be 6 or more characters long' })
+  .max(255),
+});
+
+
+export const UserZodSchema = z.object({
+  name: z
+  .string({
+    required_error: 'Name is required',
+    invalid_type_error: 'Name must be a string',
+  })
+  .min(3, { message: 'Name must be 3 or more characters long' })
+  .max(255),
+  email: z
+  .string({
+    required_error: 'Email is required',
+    invalid_type_error: 'Email must be a string',
+  })
+  .email({ message: 'Invalid email address' }
+  )
+  .max(255),
+  password: z
+  .string({
+    required_error: 'Password is required',
+    invalid_type_error: 'Password must be a string',
+  })
+  .min(6, { message: 'Password must be 6 or more characters long' })
+   .max(255),
+});
+
+
+export type ILogin = z.infer<typeof LoginZodSchema>;
+
+export type IUser = z.infer<typeof UserZodSchema>;
 
 export interface IFullUser extends IUser {
   id: string,
