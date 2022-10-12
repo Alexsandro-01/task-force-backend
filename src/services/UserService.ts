639fa1622d28467498283bdc12fa39d304718a4f
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2022-10-12 16:24:42 -0300

    feat: :sparkles: Introdução do fluxo para a criação do token no login

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,9 +1,10 @@
 /* eslint-disable @typescript-eslint/ban-types */
-import { ILogin, IUser, LoginZodSchema } from '../interfaces/IUser';
+import { ILogin, ILogedUser, IUser, LoginZodSchema } from '../interfaces/IUser';
 import { IUserService } from '../interfaces/IUserService';
 import userModel from '../database/models/Users';
 import Validate from '../validations/Validate';
 import ValidationError from '../errors/ValidationError';
+import makeToken from '../utils/jwt';
 
 class UserService implements IUserService {
   async create(_payload: IUser): Promise<userModel> {
@@ -11,7 +12,7 @@ class UserService implements IUserService {
     return response;
   }
 
-  async login(_payload: ILogin): Promise<userModel | null> {
+  async login(_payload: ILogin): Promise<ILogedUser> {
     const parsedLoginData = LoginZodSchema.safeParse(_payload);
 
     if (!parsedLoginData.success) {
@@ -27,8 +28,14 @@ class UserService implements IUserService {
     }
 
     await Validate.password(_payload.password, response?.password as string);
+    const token = makeToken({
+      key: response?.id as string,
+    });
 
-    return response;
+    return {
+      name: response?.name as string,
+      token,
+    };
   }
 }
 
