0523149c0d6d68906605e36ac5df9696fe614387
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2022-10-12 19:51:17 -0300

    refactor: :recycle: divisão do server do express em dois arquivos para facilitar os tests

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import api from './server';
+
+
+const PORT = 3000;
+
+api.listen(PORT, () => console.log(`Listen at port: ${PORT}`));
--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,6 @@ import 'express-async-errors';
 import userRoute from './routes/userRoutes';
 import errorMidlleware from './middlewares/ErrorMidlleware';
 
-const PORT = 3000;
-
 const api = express();
 
 api.use(express.json());
@@ -13,4 +11,5 @@ api.use(express.json());
 api.use('/user', userRoute);
 
 api.use(errorMidlleware);
-api.listen(PORT, () => console.log(`Listen at port: ${PORT}`));
+
+export default api;
