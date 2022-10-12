1da8745c83fa5b0af21e377bb1c00dbbdfc15a07
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2022-10-12 15:17:24 -0300

    feat: :sparkles: adiciona a validação de password no login do user

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -2,6 +2,8 @@
 import { ILogin, IUser, LoginZodSchema } from '../interfaces/IUser';
 import { IUserService } from '../interfaces/IUserService';
 import userModel from '../database/models/Users';
+import Validate from '../validations/Validate';
+import ValidationError from '../errors/ValidationError';
 
 class UserService implements IUserService {
   async create(_payload: IUser): Promise<userModel> {
@@ -19,6 +21,12 @@ class UserService implements IUserService {
     const response = await userModel.findOne({
       where: { email: _payload.email }
     });
+
+    if (!response) {
+      ValidationError.Unauthorized();
+    }
+
+    await Validate.password(_payload.password, response?.password as string);
 
     return response;
   }
