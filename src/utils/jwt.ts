a06b1c3bf37c824010a782a73a26c6c2e6b70ccb
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2022-10-22 11:15:05 -0300

    feat: :sparkles: Adiciona função para verificar token jwt

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,13 +1,25 @@
 import fs from 'fs/promises';
+import ValidationError from '../errors/ValidationError';
 import jwt from 'jsonwebtoken';
+import { tokenData } from '../interfaces/IUser';
 
 const key = 'jwt.key';
 
-async function makeToken(payload: { key: string }): Promise<string> {
+export async function makeToken(payload: { key: string }): Promise<string> {
   const secret = await fs.readFile(key, 'utf-8');
   const token = jwt.sign(payload, secret);
-
+  
   return token;
 }
 
-export default makeToken;
+export async function veryfyToken(token: string): Promise<tokenData | undefined> {
+  const secret = await fs.readFile(key, 'utf-8');
+  try {
+    const data: any = jwt.verify(token, secret);
+    return data as tokenData;
+
+  } catch (error) {
+    ValidationError.Unauthorized('Token inválido ou ausente');
+  }
+}
+
