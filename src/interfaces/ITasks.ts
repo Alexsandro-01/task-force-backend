19bc019efc568408c6b6bb22a23afa505c0c60c1
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2022-11-02 19:10:11 -0300

    feat: :label: Type de atualização de task

--- src/interfaces/ITasks.ts
+++ src/interfaces/ITasks.ts
@@ -15,4 +15,21 @@ export const CreateTaskZodSchema = z.object({
   })
 });
 
+export const UpdatetaskZodSchema = z.object({
+  task: z.
+  string({
+    required_error: 'Task is required',
+    invalid_type_error: 'Task must be a string',
+  })
+  .min(1, { message: 'Task must be 1 or more characters' })
+  .max(255, { message: 'Task need to be smaller than 255 characters'})
+  .optional(),
+  active: z.boolean({
+    required_error: 'Active is required',
+    invalid_type_error: 'Active must be a boolean'
+  })
+  .optional()
+});
+
 export type ICreateTask = z.infer<typeof CreateTaskZodSchema>
+export type IUpdateTask = z.infer<typeof UpdatetaskZodSchema>
