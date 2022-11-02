f7d48b9010c10f1c2be45c1fd551420cac6334e5
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2022-11-02 19:05:29 -0300

    feat: :sparkles: Service para update de tasks

--- src/interfaces/ITasksService.ts
+++ src/interfaces/ITasksService.ts
@@ -1,8 +1,12 @@
-import { ICreateTask } from './ITasks';
+import { ICreateTask, IUpdateTask } from './ITasks';
 import Users from '../database/models/Users';
 
 export interface ITaskService {
   create(_payload: ICreateTask, _bearerToken: string): Promise<void>,
+  updateTask(
+    _payload: IUpdateTask,
+    _bearerToken: string | undefined,
+    _id: string): Promise<string | undefined>,
   getUserById(_id: string): Promise<Users | null | undefined>,
   checkToken(_bearerToken: string): string
 }
